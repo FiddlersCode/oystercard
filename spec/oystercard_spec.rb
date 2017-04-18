b7fee3b9e849d164607f9f104e029664e4c5e7fc
Paula <pmuldoon@umich.edu>
2017-04-18 15:27:31 +0100

    defined balance method, set return value to 0

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,8 @@
 require 'oystercard'
 describe Oystercard do
+  it 'tests that a new instance of Oystercard has a balance of zero' do
+    card = Oystercard.new
+    expect(card.balance).to eq 0
+  end
 
 end
