8ac34247933180b6b9c976c728dc482663b44b56
Paula <pmuldoon@umich.edu>
2017-04-18 16:55:43 +0100

    added MAX_VALUE constant, green

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,13 @@
 class Oystercard
   attr_reader :balance
-
+  MAX_VALUE = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
-    fail "Max balance £90" if @balance + amount >= 90
+    fail "Max balance £90" if @balance + amount >= MAX_VALUE
     @balance += amount
   end
 end
