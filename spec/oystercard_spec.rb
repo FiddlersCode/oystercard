65fa75f373ee43c503510042b83cbdd120671b1e
Paula <pmuldoon@umich.edu>
2017-04-18 16:12:55 +0100

    further refactoring of spec file initial test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,10 +1,10 @@
 require 'oystercard'
 describe Oystercard do
-  it 'tests that a new instance of Oystercard has a balance of zero' do
+  it 'tests that an instance of Oystercard has a balance of zero' do
     expect(subject.balance).to eq 0
   end
 
-  it 'tests that a new instance of Oystercard can be initalized with a default value of 3' do
+  it 'tests that an instance of Oystercard can be initalized with a default value of 3' do
     card2 = Oystercard.new(3)
     expect(card2.balance).to eq 3
   end
